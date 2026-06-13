7dfc7881f4b16a6d3c590f2ff11ebf7dfd0a6ca8
Paulo Roberto <evans_sp@hotmail.com>
2026-06-13 17:54:57 -0300

    refactor: renomear parâmetro do UpdateTransactionUseCase para português

--- app/src/main/java/com/example/financeapp/domain/usecase/UpdateTransactionUseCase.kt
+++ app/src/main/java/com/example/financeapp/domain/usecase/UpdateTransactionUseCase.kt
@@ -3,8 +3,8 @@ package com.example.financeapp.domain.usecase
 import com.example.financeapp.domain.model.Transaction
 import com.example.financeapp.domain.repository.TransactionRepository
 
-class UpdateTransactionUseCase (private val repository: TransactionRepository) {
-    suspend operator fun invoke(transaction: Transaction){
-        repository.updateTransacrion(transaction)
+class UpdateTransactionUseCase(private val repositorio: TransactionRepository) {
+    suspend operator fun invoke(transacao: Transaction) {
+        repositorio.atualizarTransacao(transacao)
     }
 }
