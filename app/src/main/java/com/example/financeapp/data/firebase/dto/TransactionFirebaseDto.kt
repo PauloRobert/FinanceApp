fd956c273616b1ef358f3d0f6c1b038a801b1682
Paulo Roberto <evans_sp@hotmail.com>
2026-06-16 14:05:14 -0300

    chore: remover arquivos de Firebase (datasource, dto, repository, mapper)

--- app/src/main/java/com/example/financeapp/data/firebase/dto/TransactionFirebaseDto.kt
+++
@@ -1,9 +0,0 @@
-package com.example.financeapp.data.firebase.dto
-
-data class TransactionFirebaseDto (
-    val id: String = "",
-    val description: String = "",
-    val amount: Double = 0.0,
-    val date: String = "",
-    val type: String = ""
-)
